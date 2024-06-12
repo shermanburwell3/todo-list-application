01ae86c7e88daa6cda3db1be2c5fa9ded96394b2
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-11 20:42:50 -0500

    Added pseudo code to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,26 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+    // Check length of taskList array and return number based on that
 
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    //Create
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
+    // create ul based on localStorage and ul.sortable()
+
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    // get modal form data and return the text input. this will be called by event listener
 
 }
 
@@ -29,6 +34,7 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+    // ul.sortable() advanced
 
 }
 
