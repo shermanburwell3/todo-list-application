e04b20b48a07f6e6a5c0a79b0111283e123f7ba7
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-12 17:06:20 -0500

    Completed initial testing of createTaskCard function, adding code to and testing generateTaskId.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,16 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     // Check length of taskList array and return number based on that
+    if (taskList) {
+
+    // Generate an ID based on the length of existing tasks
+        let i = taskList.length;
+        return i;
+    }
+    else {
+        let i = 0;
+        return i;
+    }
 
 }
 
@@ -12,10 +22,11 @@ function generateTaskId() {
 function createTaskCard(task) {
 
     // Create elements for task card
-    taskCard = $('div');
-    taskCard.attr('id', task.id);
+    taskCard = $('<div>');
+    taskCard.attr('id', generateTaskId);
+    taskCard.attr('style', 'background-color: white;')
     taskCardTitle = $('<h3>').text(task.title);
-    taskCardDueDate = $('<p>').text(task.dueDate);
+    taskCardDueDate = $('<p>').text("Due by EoD: " +task.dueDate);
     taskCardBody = $('<p>').text(task.body);
     deleteButton = $('<button>').text("Delete");
 
@@ -24,7 +35,7 @@ function createTaskCard(task) {
     taskCard.append(taskCardBody);
     taskCard.append(deleteButton);
 
-    $('#to-do').append(taskCard);
+    $('#todo-cards').append(taskCard);
     
 
 }
@@ -64,6 +75,15 @@ $(document).ready(function () {
 createTaskCard({
     id: 0,
     title: "Build more JS",
+    dueDate: "06/12/2024",
+    body: "I need to get good at jquery fast."
+
+
+});
+
+createTaskCard({
+    id: 0,
+    title: "Build even more JS",
     dueDate: "06/12/2024",
     body: "I need to get good at jquery fast."
 
